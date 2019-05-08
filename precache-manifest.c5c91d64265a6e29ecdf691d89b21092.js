30872ea40747d3b547bba99cba0531ba45dd3f8b
Alex <alexiv1102@gmail.com>
2019-05-08 16:09:55 +0300

    Updates

---
+++ precache-manifest.c5c91d64265a6e29ecdf691d89b21092.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "73b94160db9479545647",
+    "url": "/weather-app-canvas/static/js/runtime~main.0ae1b1d8.js"
+  },
+  {
+    "revision": "bf1e941747ceee6ff48f",
+    "url": "/weather-app-canvas/static/js/main.7d16a7d0.chunk.js"
+  },
+  {
+    "revision": "192a684d646f174fbb03",
+    "url": "/weather-app-canvas/static/js/2.dcb409d0.chunk.js"
+  },
+  {
+    "revision": "2ecb0ca508bcd9e28955a3e10e9945c3",
+    "url": "/weather-app-canvas/index.html"
+  }
+];
